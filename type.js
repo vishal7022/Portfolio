e6b09e4298a84a2dc6d91c42d4c865841516d80a
Vishal Baitha <147191447+vishal7022@users.noreply.github.com>
2023-12-13 21:39:24 +0530

    it's me

---
+++ type.js
@@ -0,0 +1,10 @@
+// body{
+// <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js">
+
+// script
+//     var typed = new Typed('#element', {
+//       strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+//       typeSpeed: 50,
+//     });
+  
+//         }
